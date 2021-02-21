c9192254bffacdcd5de6fc9be7a4c1616b38e9a2
JuneKim0712 <Junwoo.kim0712@gmail.com>
2021-02-20 23:44:49 -0800

    middle checkpoint, progressed minimax and nextmove function

--- TTT_HH.py
+++ TTT_HH.py
@@ -2,6 +2,7 @@ import numpy as np
 import pygame
 
 
+#converting player input to refinable coordinate value
 def placement(coordinate):
     x=coordinate[0]
     y=coordinate[1]
@@ -28,6 +29,7 @@ def placement(coordinate):
             return (405, 405)
 
 
+#function that check is the current player have won
 def is3(c):
     if (205, 205) in c:
         if (5, 5) in c and (405, 405) in c:
@@ -49,6 +51,48 @@ def is3(c):
         elif (205, 405) in c and (405, 405) in c:
             return 1
     return 0
+
+
+#nextmove for AI
+def nextmove(_1, _0):
+    stm=cas(_1+_0)
+    amove=[(5, 5), -1, 0]
+    for i in stm:
+        inax=minimax((_1+[i]).copy(), _0.copy(), '_1', 0)
+        if inax[0] >= amove[1] and inax[1] <= amove[2]:
+            amove=[i, inax[0], inax[1]]
+    return amove[0]
+
+
+#1=win, -1=lost, draw=0, returns coordinate, (win, lose or draw)
+def minimax(_1, _0, turn, tree):
+    if turn=='_0': turn='_1'
+    else: turn='_0'
+    tree += 1
+    if is3(_1): return (1, tree, turn)
+    elif is3(_0): return (-1, tree, turn)
+    elif tree == 9: return (0, tree, turn)
+    stm=cas(_1+_0)
+    list_wld=[]
+    for i in stm:
+        result=minimax((_1+[i]).copy(), _0, turn, tree.copy())
+        if result == 1 and turn=='_1':
+            return (result, tree, turn)
+        list_wld.append(result)
+    list_wld.sort()
+    if turn=='_1': wld=list_wld[-1]
+    else: wld=list_wld[0]
+    return (wld, tree, turn)
+
+
+#function that shows avaible space for next move
+def cas(list_):
+    avsp=[]
+    for i in [(5, 5), (205, 5), (405, 5), (5, 205), (205, 205), (405, 205), (5, 405), (205, 405), (405, 405)]:
+        if not i in list_: avsp.append(i)
+        else: pass
+    return avsp.copy()
+
 
 def ttt():
     pygame.init()
@@ -78,12 +122,10 @@ def ttt():
                     if is3(_0):
                         print('circle won')
                         open=False
-                    
                 else:
-                    po=placement(pygame.mouse.get_pos())
-                    if not po in _0 and not po in _1:
-                        player=0
-                        _1.append(po)
+                    po=nextmove(_1.copy(), _0.copy())
+                    player=0
+                    _1.append(po)
                     if is3(_1):
                         print('cross won')
                         open=False
